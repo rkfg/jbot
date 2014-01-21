fea8734d342f5241893ccbdaf72423b590311989
eurekafag <eurekafag@eureka7.ru>
2014-01-21 22:19:25 +0400

    Title request timeout.

--- src/main/java/me/rkfg/xmpp/bot/Utils.java
+++ src/main/java/me/rkfg/xmpp/bot/Utils.java
@@ -1,11 +1,16 @@
 package me.rkfg.xmpp.bot;
 
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 public class Utils {
+    private static int timeout = 10000;
+
     public static HttpClient getHTTPClient() {
-        return HttpClientBuilder.create()
+        RequestConfig config = RequestConfig.custom().setConnectionRequestTimeout(timeout).setConnectTimeout(timeout)
+                .setSocketTimeout(timeout).build();
+        return HttpClientBuilder.create().setDefaultRequestConfig(config)
                 .setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36")
                 .build();
     }
