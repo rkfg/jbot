3e74b3bcb34e2120d7dbb0a6d1b6570ee64676c2
eurekafag <eurekafag@eureka7.ru>
2014-01-22 22:55:44 +0400

    Man command plugin.

---
+++ src/main/java/me/rkfg/xmpp/bot/plugins/ManCommandPlugin.java
@@ -0,0 +1,51 @@
+package me.rkfg.xmpp.bot.plugins;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+
+import me.rkfg.xmpp.bot.Main;
+
+import org.jivesoftware.smack.packet.Message;
+
+import ru.ppsrk.gwt.client.ClientAuthenticationException;
+import ru.ppsrk.gwt.client.LogicException;
+import ru.ppsrk.gwt.shared.SharedUtils;
+
+public class ManCommandPlugin extends CommandPlugin {
+
+    @Override
+    public String processCommand(Message message, Matcher matcher) throws ClientAuthenticationException, LogicException {
+        if (matcher.group(3) == null || matcher.group(3).isEmpty()) {
+            List<String> commands = new LinkedList<String>();
+            for (MessagePluginImpl plugin : Main.plugins) {
+                if (plugin instanceof CommandPlugin) {
+                    commands.addAll(((CommandPlugin) plugin).getCommand());
+                }
+            }
+            return "доступные команды: " + SharedUtils.join(commands, ", ");
+        } else {
+            String cmd = matcher.group(3);
+            for (MessagePluginImpl plugin : Main.plugins) {
+                if (plugin instanceof CommandPlugin) {
+                    if (((CommandPlugin) plugin).getCommand().contains(cmd)) {
+                        return plugin.getManual();
+                    }
+                }
+            }
+        }
+        return "команда не найдена.";
+    }
+
+    @Override
+    public List<String> getCommand() {
+        return Arrays.asList("man", "ьфт", "ман");
+    }
+
+    @Override
+    public String getManual() {
+        return "получить справку по команде.\nФормат: <команда>\nПример: " + PREFIX + "man man";
+    }
+
+}
