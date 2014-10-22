017a84cd376a2b2e3c510ecdcac69a6b5d8655a6
neexee <neexee>
2014-10-22 22:02:33 +0700

    New plugins: AnonEcho, Timer

---
+++ src/main/java/me/rkfg/xmpp/bot/plugins/AnonEchoPlugin.java
@@ -0,0 +1,10 @@
+package me.rkfg.xmpp.bot.plugins;
+
+/**
+ * User: violetta
+ * Date: 10/22/14
+ * Time: 8:04 PM
+ */
+public class AnonEchoPlugin
+{
+}
---
+++ src/main/java/me/rkfg/xmpp/bot/plugins/TimerPlugin.java
@@ -0,0 +1,10 @@
+package me.rkfg.xmpp.bot.plugins;
+
+/**
+ * User: violetta
+ * Date: 10/22/14
+ * Time: 8:17 PM
+ */
+public class TimerPlugin
+{
+}
