bc5c06f266c42dd70c8e15e14dd93683e30e5bcf
rkfg <rkfg@rkfg.me>
2015-01-07 16:42:24 +0300

    Anti-highlight function added.

--- src/main/java/me/rkfg/xmpp/bot/plugins/MessagePluginImpl.java
+++ src/main/java/me/rkfg/xmpp/bot/plugins/MessagePluginImpl.java
@@ -1,5 +1,8 @@
 package me.rkfg.xmpp.bot.plugins;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Message.Type;
 import org.jivesoftware.smack.util.StringUtils;
@@ -37,5 +40,26 @@ public abstract class MessagePluginImpl implements MessagePlugin {
     @Override
     public String getManual() {
         return "справка по плагину отсутствует.";
+    }
+
+    public String antiHighlight(String nick) {
+        List<Character> eng = Arrays.asList('a', 'c', 'e', 'k', 'o', 'p', 'x', 'y');
+        List<Character> rus = Arrays.asList('а', 'с', 'е', 'к', 'о', 'р', 'х', 'у');
+        StringBuilder result = new StringBuilder(nick.length());
+        for (int i = 0; i < nick.length(); i++) {
+            char nickChar = nick.charAt(i);
+            int engIndex = eng.indexOf(nickChar);
+            if (engIndex >= 0) {
+                result.append(rus.get(engIndex));
+            } else {
+                int rusIndex = rus.indexOf(nickChar);
+                if (rusIndex >= 0) {
+                    result.append(eng.get(rusIndex));
+                } else {
+                    result.append(nickChar);
+                }
+            }
+        }
+        return result.toString();
     }
 }
