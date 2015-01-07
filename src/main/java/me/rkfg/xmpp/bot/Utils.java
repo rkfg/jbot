9e15838268dd861080db5dcce8814d6850b0c662
rkfg <rkfg@rkfg.me>
2015-01-07 03:19:44 +0300

    Custom timeout for http client.

--- src/main/java/me/rkfg/xmpp/bot/Utils.java
+++ src/main/java/me/rkfg/xmpp/bot/Utils.java
@@ -5,13 +5,17 @@ import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 public class Utils {
-    private static int timeout = 10000;
+    private static int TIMEOUT = 10000;
 
-    public static HttpClient getHTTPClient() {
+    public static HttpClient getHTTPClient(int timeout) {
         RequestConfig config = RequestConfig.custom().setConnectionRequestTimeout(timeout).setConnectTimeout(timeout)
                 .setSocketTimeout(timeout).build();
         return HttpClientBuilder.create().setDefaultRequestConfig(config)
                 .setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36")
                 .build();
+    }
+
+    public static HttpClient getHTTPClient() {
+        return getHTTPClient(TIMEOUT);
     }
 }
