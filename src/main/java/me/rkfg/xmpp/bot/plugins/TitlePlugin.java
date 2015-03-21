bf4fa174d385e808716dba650b1653fb2ba37cfb
rkfg <rkfg@rkfg.me>
2015-03-21 13:54:08 +0300

    Image content recognizer added.

--- src/main/java/me/rkfg/xmpp/bot/plugins/TitlePlugin.java
+++ src/main/java/me/rkfg/xmpp/bot/plugins/TitlePlugin.java
@@ -1,14 +1,24 @@
 package me.rkfg.xmpp.bot.plugins;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.InterruptedIOException;
 import java.net.IDN;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.http.client.ClientProtocolException;
 import org.jivesoftware.smack.packet.Message;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.jsoup.Jsoup;
+import org.jsoup.UnsupportedMimeTypeException;
 import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +29,8 @@ public class TitlePlugin extends MessagePluginImpl {
     private static final int MAX_BODY_SIZE = 102400;
     private static final int TIMEOUT = 10000;
     private Logger log = LoggerFactory.getLogger(getClass());
+
+    private List<String> imageMime = Arrays.asList("image/png", "image/jpg", "image/jpeg", "image/gif", "image/bmp");
 
     @Override
     public Pattern getPattern() {
@@ -41,9 +53,42 @@ public class TitlePlugin extends MessagePluginImpl {
         } catch (InterruptedIOException e) {
             log.warn("Timed out loading URL: {}", url);
         } catch (ClientProtocolException e) {
-            //
+            e.printStackTrace();
+        } catch (UnsupportedMimeTypeException e) {
+            if (imageMime.contains(e.getMimeType())) {
+                log.info("Image found in URL: {}", e.getUrl());
+                try {
+                    int contentLength = new URL(e.getUrl()).openConnection().getContentLength();
+                    if (contentLength > 5120000) {
+                        log.warn("Content is too big ({})!", contentLength);
+                        return null;
+                    }
+                    URLConnection conn = new URL("http://127.0.0.1:5010/classify_url?imageurl=" + URLEncoder.encode(e.getUrl(), "UTF-8"))
+                            .openConnection();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = in.readLine()) != null) {
+                        sb.append(line);
+                    }
+                    JSONObject betsObj = new JSONObject(sb.toString());
+                    if (betsObj.has("bets")) {
+                        StringBuilder result = new StringBuilder();
+                        JSONArray betsArr = betsObj.getJSONArray("bets");
+                        for (int i = 0; i < betsArr.length(); i++) {
+                            if (result.length() > 0) {
+                                result.append(", ");
+                            }
+                            result.append(betsArr.get(i));
+                        }
+                        return "На картинке, возможно: " + result.toString();
+                    }
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
         } catch (IOException e) {
-            //
+            e.printStackTrace();
         }
         return null;
     }
