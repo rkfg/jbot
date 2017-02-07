a0855cbb7c3c66dbf55498ba4c26b60d3d90c44d
skfg <asukafag@behind.computer>
2017-02-07 18:08:46 +0300

    Fixed URL decoding.

--- src/main/java/me/rkfg/xmpp/bot/plugins/CustomSearchPlugin.java
+++ src/main/java/me/rkfg/xmpp/bot/plugins/CustomSearchPlugin.java
@@ -7,7 +7,6 @@ package me.rkfg.xmpp.bot.plugins;
 
 import java.io.IOException;
 import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
