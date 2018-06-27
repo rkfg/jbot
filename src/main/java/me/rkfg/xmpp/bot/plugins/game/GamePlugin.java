fe5ac8d4e3ce0a5e721e30aeb49c23ea48191748
rkfg <rkfg@rkfg.me>
2018-06-27 13:43:08 +0300

    Refactor commands

--- src/main/java/me/rkfg/xmpp/bot/plugins/game/GamePlugin.java
+++ src/main/java/me/rkfg/xmpp/bot/plugins/game/GamePlugin.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -34,6 +36,9 @@ public class GamePlugin extends CommandPlugin {
     }
 
     private GameState state = GameState.GATHER;
+    private static final Optional<String> NORESULT = Optional.empty();
+    private static final Optional<String> SLEEP_HELP = Optional
+            .of("Неверный режим сна. Укажите значение цифрой: 0 — глубокий сон, 1 — сон вполглаза, 2 — бодрствование.");
 
     @Override
     public void init() {
@@ -42,13 +47,13 @@ public class GamePlugin extends CommandPlugin {
             @Override
             public void run() {
                 synchronized (GamePlugin.this) {
-                    for (Player player : players.values()) {
-                        player.enqueueEvent(new TickEvent());
-                    }
+                    players.values().forEach(p -> p.enqueueEvent(new TickEvent()));
                 }
             }
         }, TimeUnit.SECONDS.toMillis(5), TimeUnit.SECONDS.toMillis(5));
     }
+
+    private Map<String, Function<Stream<String>, Optional<String>>> handlers = new HashMap<>();
 
     @Override
     public synchronized String processCommand(Message message, Matcher matcher) throws LogicException, ClientAuthException {
@@ -58,46 +63,51 @@ public class GamePlugin extends CommandPlugin {
             args = Stream.of(argsStr.split(" ")).filter(c -> !c.isEmpty()).collect(Collectors.toList());
         }
         IPlayer player = getCurrentPlayer(message);
+        handlers.put("", a -> {
+            player.dumpStats();
+            return NORESULT;
+        });
+        handlers.put("спать", a -> {
+            Optional<String> arg = a.findFirst();
+            if (arg.isPresent()) {
+                try {
+                    SleepType sleepType = arg.map(Integer::valueOf).filter(v -> v >= 0 && v < 3).map(v -> SleepType.values()[v])
+                            .orElseThrow(NumberFormatException::new);
+                    player.enqueueEvent(new SetSleepEvent(sleepType, player));
+                } catch (NumberFormatException e) {
+                    return SLEEP_HELP;
+                }
+            } else {
+                return player.findEffect(SleepEffect.TYPE).map(
+                        e -> "Выбранный режим сна: " + e.getAttribute(SleepEffect.SLEEP_TYPE_ATTR).map(SleepType::getLocalized).orElse(""));
+            }
+            return Optional.empty();
+        });
         if (!player.isAlive()) {
             player.dumpStats();
             player.log("Вы умерли и не можете играть далее.");
         } else {
-            if (state == GameState.GATHER && player.listEffects().isEmpty()) {
-                ((Player) player).setName("Test name");
-                player.enqueueEvent(new SetSleepEvent(SleepType.DEEP, Player.WORLD));
-                StatsEffect statsEffectFat = new StatsEffect("fat", "жиробасина", Player.WORLD);
-                statsEffectFat.setStatChange(Player.ATK, 1);
-                statsEffectFat.setStatChange(Player.DEF, -1);
-                StatsEffect statsEffectAlco = new StatsEffect("alcoholic", "алкашня", Player.WORLD);
-                statsEffectAlco.setStatChange(Player.DEF, -1);
-                statsEffectAlco.setStatChange(Player.PRT, -1);
-                statsEffectAlco.addEffect(new NoGuardSleepEffect(Player.WORLD));
-                player.enqueueEvents(new EffectEvent(statsEffectFat), new EffectEvent(statsEffectAlco),
-                        new EffectEvent(new BleedEffect(player, 2)));
-            }
-            if (args.isEmpty()) {
-                player.dumpStats();
-            } else {
-                String cmd = args.get(0);
-                if ("спать".equals(cmd)) {
-                    if (args.size() == 1) {
-                        player.findEffect(SleepEffect.TYPE).ifPresent(e -> player.log("Выбранный режим сна: "
-                                + e.getAttribute(SleepEffect.SLEEP_TYPE_ATTR).map(SleepType::getLocalized).orElse("")));
-                    }
-                    if (args.size() == 2) {
-                        try {
-                            Integer sleepInt = Integer.valueOf(args.get(1));
-                            if (sleepInt < 0 || sleepInt > 2) {
-                                return "неверный режим сна";
-                            }
-                            player.enqueueEvent(new SetSleepEvent(SleepType.values()[sleepInt], player));
-                        } catch (NumberFormatException e) {
-                        }
-                    }
-                }
-            }
+            initPlayer(player);
+            String cmd = args.stream().findFirst().orElse("");
+            return handlers.get(cmd).apply(args.stream().skip(1)).orElseGet(player::getLog);
         }
         return player.getLog();
+    }
+
+    public void initPlayer(IPlayer player) {
+        if (state == GameState.GATHER && player.listEffects().isEmpty()) {
+            ((IMutablePlayer) player).setName("Test name");
+            player.enqueueEvent(new SetSleepEvent(SleepType.DEEP, IPlayer.WORLD));
+            StatsEffect statsEffectFat = new StatsEffect("fat", "жиробасина", IPlayer.WORLD);
+            statsEffectFat.setStatChange(Player.ATK, 1);
+            statsEffectFat.setStatChange(Player.DEF, -1);
+            StatsEffect statsEffectAlco = new StatsEffect("alcoholic", "алкашня", IPlayer.WORLD);
+            statsEffectAlco.setStatChange(Player.DEF, -1);
+            statsEffectAlco.setStatChange(Player.PRT, -1);
+            statsEffectAlco.addEffect(new NoGuardSleepEffect(IPlayer.WORLD));
+            player.enqueueEvents(new EffectEvent(statsEffectFat), new EffectEvent(statsEffectAlco));
+            player.enqueueEvent(new EffectEvent(new BleedEffect(IPlayer.WORLD, 2)));
+        }
     }
 
     public IPlayer getCurrentPlayer(Message message) {
