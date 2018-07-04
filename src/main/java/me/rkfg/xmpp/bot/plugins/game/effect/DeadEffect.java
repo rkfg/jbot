a869c235d56ab635c45476e41bbeca00fd9247f6
rkfg <rkfg@rkfg.me>
2018-07-04 15:45:12 +0300

    Add dead message

--- src/main/java/me/rkfg/xmpp/bot/plugins/game/effect/DeadEffect.java
+++ src/main/java/me/rkfg/xmpp/bot/plugins/game/effect/DeadEffect.java
@@ -1,5 +1,7 @@
 package me.rkfg.xmpp.bot.plugins.game.effect;
 
+import static me.rkfg.xmpp.bot.plugins.game.misc.Attrs.*;
+
 import java.util.Collection;
 
 import me.rkfg.xmpp.bot.plugins.game.World;
@@ -16,6 +18,7 @@ public class DeadEffect extends AbstractEffect {
 
     @Override
     public Collection<IEvent> processEvent(IEvent event) {
+        target.as(PLAYER_OBJ).ifPresent(p -> event.getSource().log("%s мёртв.", p.getName()));
         return cancelEvent(); // dead man reacts to no events
     }
 
