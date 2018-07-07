11d69c6d6fc337463732c8be1c910fbeed80b3f4
rkfg <rkfg@rkfg.me>
2018-07-07 12:19:39 +0300

    At least one attack in the battle must succeed
    This is capped by 10000 rolls so that it may eventually end in a draw.
    Attack events only rerolled if both of them didn't succeed due to
    atk/def or str/prt failed checks so that any further event-cancelling
    mechanics aren't affected. I.e. a cancelled (via effects) successful
    attack doesn't cause a reroll even if the other attack failed due to
    initial checks.

--- src/main/java/me/rkfg/xmpp/bot/plugins/game/event/BattleEvent.java
+++ src/main/java/me/rkfg/xmpp/bot/plugins/game/event/BattleEvent.java
@@ -2,13 +2,13 @@ package me.rkfg.xmpp.bot.plugins.game.event;
 
 import static me.rkfg.xmpp.bot.plugins.game.misc.Attrs.*;
 
-import me.rkfg.xmpp.bot.plugins.game.IGameObject;
 import me.rkfg.xmpp.bot.plugins.game.IPlayer;
 import me.rkfg.xmpp.bot.plugins.game.effect.BattleFatigueEffect;
 import me.rkfg.xmpp.bot.plugins.game.misc.Utils;
 
 public class BattleEvent extends AbstractEvent {
 
+    private static final int ROLL_LIMIT = 10000;
     public static final String TYPE = "battle";
 
     public BattleEvent(IPlayer attacker, IPlayer defender) {
@@ -27,8 +27,17 @@ public class BattleEvent extends AbstractEvent {
             }
             attacker.log(String.format("Вы нападаете на %s!", Utils.getPlayerName(defender)));
             defender.log(String.format("%s нападает на вас!", Utils.getPlayerName(attacker)));
-            battleTurn(attacker, defender);
-            battleTurn(defender, attacker);
+            BattleAttackEvent attackEvent = null;
+            BattleAttackEvent defenceEvent = null;
+            int rollLimit = ROLL_LIMIT;
+            // roll until at least one attack succeeds
+            while (rollLimit > 0 && (attackEvent == null || !attackEvent.isSuccessful()) && (defenceEvent == null || !defenceEvent.isSuccessful())) {
+                attackEvent = new BattleAttackEvent(attacker, defender);
+                defenceEvent = new BattleAttackEvent(defender, attacker);
+                --rollLimit;
+            }
+            battleTurn(attackEvent);
+            battleTurn(defenceEvent);
             attacker.enqueueEvent(new BattleEndsEvent(attacker, defender));
             defender.enqueueEvent(new BattleEndsEvent(attacker, defender));
             String endMessage = String.format("Бой между %s и %s завершён.", Utils.getPlayerName(attacker), Utils.getPlayerName(defender));
@@ -43,23 +52,24 @@ public class BattleEvent extends AbstractEvent {
         return event;
     }
 
-    private void battleTurn(IGameObject srcPlayer, IGameObject tgtPlayer) {
-        BattleAttackEvent attackEvent = new BattleAttackEvent(srcPlayer, tgtPlayer);
-        if (tgtPlayer.enqueueEvent(attackEvent)
-                && tgtPlayer.as(PLAYER_OBJ).flatMap(IPlayer::getArmor).map(a -> a.enqueueEvent(attackEvent)).orElse(true)
-                && srcPlayer.as(PLAYER_OBJ).flatMap(IPlayer::getWeapon).map(a -> a.enqueueEvent(attackEvent)).orElse(true)
-                && srcPlayer.enqueueEvent(attackEvent)) {
-            if (attackEvent.isSuccessful()) {
-                srcPlayer.log("Атака достигает цели и наносит " + attackEvent.getDamage() + " урона!");
-                tgtPlayer.log("Соперник наносит вам " + attackEvent.getDamage() + " урона!");
-                final StatsEvent statsEvent = new StatsEvent();
-                statsEvent.setSource(srcPlayer);
-                tgtPlayer.enqueueEvent(statsEvent.setAttributeChain(HP, -attackEvent.getDamage()));
-            } else {
-                srcPlayer.log("Вы пытаетесь поразить соперника, но промахиваетесь!");
-                tgtPlayer.log("Соперник пытается нанести удар, но промахивается!");
+    private void battleTurn(BattleAttackEvent attackEvent) {
+        attackEvent.getSource().as(PLAYER_OBJ).ifPresent(srcPlayer -> attackEvent.getTarget().as(PLAYER_OBJ).ifPresent(tgtPlayer -> {
+            if (tgtPlayer.enqueueEvent(attackEvent)
+                    && tgtPlayer.as(PLAYER_OBJ).flatMap(IPlayer::getArmor).map(a -> a.enqueueEvent(attackEvent)).orElse(true)
+                    && srcPlayer.as(PLAYER_OBJ).flatMap(IPlayer::getWeapon).map(a -> a.enqueueEvent(attackEvent)).orElse(true)
+                    && srcPlayer.enqueueEvent(attackEvent)) {
+                if (attackEvent.isSuccessful()) {
+                    srcPlayer.log("Атака достигает цели и наносит " + attackEvent.getDamage() + " урона!");
+                    tgtPlayer.log("Соперник наносит вам " + attackEvent.getDamage() + " урона!");
+                    final StatsEvent statsEvent = new StatsEvent();
+                    statsEvent.setSource(srcPlayer);
+                    tgtPlayer.enqueueEvent(statsEvent.setAttributeChain(HP, -attackEvent.getDamage()));
+                } else {
+                    srcPlayer.log("Вы пытаетесь поразить соперника, но промахиваетесь!");
+                    tgtPlayer.log("Соперник пытается нанести удар, но промахивается!");
+                }
             }
-        }
+        }));
     }
 
 }
