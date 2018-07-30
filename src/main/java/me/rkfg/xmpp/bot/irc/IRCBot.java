c4ec483ff84be3d102d69455b71b8f1e5c481ad4
rkfg <rkfg@rkfg.me>
2018-07-30 17:47:57 +0300

    Refactor out the parser, restore state on line break

--- src/main/java/me/rkfg/xmpp/bot/irc/IRCBot.java
+++ src/main/java/me/rkfg/xmpp/bot/irc/IRCBot.java
@@ -2,11 +2,7 @@ package me.rkfg.xmpp.bot.irc;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,7 +10,6 @@ import java.util.regex.Pattern;
 import javax.net.ssl.SSLSocketFactory;
 
 import org.jivesoftware.smack.chat.ChatManager;
-import org.pircbotx.Colors;
 import org.pircbotx.Configuration;
 import org.pircbotx.Configuration.Builder;
 import org.pircbotx.PircBotX;
@@ -38,7 +33,7 @@ public class IRCBot extends BotBase implements IBot {
 
     private PircBotX bot;
 
-    private static final Map<String, String> REPLACES = new HashMap<>();
+    private int maxLength;
 
     private class Listener extends ListenerAdapter {
         @Override
@@ -69,7 +64,7 @@ public class IRCBot extends BotBase implements IBot {
                         try {
                             String result = plugin.process(ircMessage, matcher);
                             if (result != null && !result.isEmpty()) {
-                                ircMessage.getOriginalMessage().respond(postprocessMessage(result));
+                                postprocessMessage(result).forEach(l -> ircMessage.getOriginalMessage().respond(l));
                                 break;
                             }
                         } catch (Exception e) {
@@ -85,25 +80,7 @@ public class IRCBot extends BotBase implements IBot {
     @Override
     protected void init() {
         super.init();
-        REPLACES.put("b", Colors.BOLD);
-        REPLACES.put("i", Colors.ITALICS);
-        REPLACES.put("u", Colors.UNDERLINE);
-        REPLACES.put("white", Colors.WHITE);
-        REPLACES.put("black", Colors.BLACK);
-        REPLACES.put("red", Colors.RED);
-        REPLACES.put("brown", Colors.BROWN);
-        REPLACES.put("magenta", Colors.MAGENTA);
-        REPLACES.put("purple", Colors.PURPLE);
-        REPLACES.put("olive", Colors.OLIVE);
-        REPLACES.put("yellow", Colors.YELLOW);
-        REPLACES.put("green", Colors.GREEN);
-        REPLACES.put("dgreen", Colors.DARK_GREEN);
-        REPLACES.put("gray", Colors.LIGHT_GRAY);
-        REPLACES.put("dgray", Colors.DARK_GRAY);
-        REPLACES.put("blue", Colors.BLUE);
-        REPLACES.put("dblue", Colors.DARK_BLUE);
-        REPLACES.put("teal", Colors.TEAL);
-        REPLACES.put("cyan", Colors.CYAN);
+        MessageParser.init();
     }
 
     @Override
@@ -116,7 +93,8 @@ public class IRCBot extends BotBase implements IBot {
                 .setNickservPassword(sm.getStringSetting("password"))
                 .addServer(sm.getStringSetting("ircServer"), sm.getIntegerSetting("ircPort"))
                 .addAutoJoinChannel("#" + sm.getStringSetting("join")).addListener(new Listener()).setAutoReconnect(true)
-                .setAutoReconnectDelay(5000).setMaxLineLength(sm.getIntegerSetting("maxLine")).setMessageDelay(100);
+                .setAutoReconnectDelay(5000).setMessageDelay(100);
+        maxLength = sm.getIntegerSetting("maxLine");
         if ("1".equals(sm.getStringSetting("ircSsl"))) {
             builder.setSocketFactory(SSLSocketFactory.getDefault());
         }
@@ -140,106 +118,17 @@ public class IRCBot extends BotBase implements IBot {
 
     @Override
     public String sendMessage(String message, String mucName) {
-        bot.sendIRC().message(mucName, postprocessMessage(message));
+        postprocessMessage(message).forEach(msg -> bot.sendIRC().message(mucName, msg));
         return null;
-    }
-
-    private String postprocessMessage(String message) {
-        message = message.replace('\n', ' ');
-        int i = 0;
-        StringBuilder result = new StringBuilder();
-        List<String> state = new LinkedList<>();
-        boolean needRestore = false;
-        while (i < message.length()) {
-            final char ch = message.charAt(i);
-            boolean codeFound = false;
-            if (ch == '<') {
-                for (Entry<String, String> r : REPLACES.entrySet()) {
-                    if (message.length() > i + r.getKey().length() + 1
-                            && message.substring(i + 1).toLowerCase().startsWith(r.getKey() + ">")) {
-                        i += r.getKey().length() + 2;
-                        result.append(r.getValue());
-                        setState(state, r.getValue(), true);
-                        codeFound = true;
-                    }
-                    if (message.length() > i + r.getKey().length() + 2
-                            && message.substring(i + 1).toLowerCase().startsWith("_" + r.getKey() + ">")) {
-                        i += r.getKey().length() + 3;
-                        result.append(r.getValue().substring(0, 1) + "," + r.getValue().substring(1));
-                        setState(state, r.getValue(), false);
-                        codeFound = true;
-                    }
-                    if (message.length() > i + r.getKey().length() + 2
-                            && message.substring(i + 1).toLowerCase().startsWith("/" + r.getKey() + ">")) {
-                        i += r.getKey().length() + 3;
-                        removeState(state, r.getValue(), true);
-                        needRestore = true;
-                        codeFound = true;
-                    }
-                    if (message.length() > i + r.getKey().length() + 3
-                            && message.substring(i + 1).toLowerCase().startsWith("/_" + r.getKey() + ">")) {
-                        i += r.getKey().length() + 4;
-                        removeState(state, r.getValue(), false);
-                        needRestore = true;
-                        codeFound = true;
-                    }
-                }
-            }
-            if (!codeFound) {
-                if (needRestore) {
-                    restoreState(state, result);
-                    needRestore = false;
-                }
-                result.append(ch);
-                ++i;
-            }
-        }
-        return result.toString();
-    }
-
-    private void removeState(List<String> state, String value, boolean foreground) {
-        if (Colors.BOLD.equals(value) || Colors.ITALICS.equals(value) || Colors.UNDERLINE.equals(value)) {
-            state.remove(value);
-        } else {
-            state.remove((foreground ? "F_" : "B_") + value.substring(1));
-        }
-    }
-
-    private void restoreState(List<String> state, StringBuilder result) {
-        result.append(Colors.NORMAL);
-        String fgColor = null;
-        String bgColor = null;
-        for (String s : state) {
-            if (fgColor == null && s.startsWith("F_")) {
-                fgColor = s.substring(2);
-            } else if (bgColor == null && s.startsWith("B_")) {
-                bgColor = s.substring(2);
-            } else {
-                result.append(s);
-            }
-        }
-        if (fgColor != null || bgColor != null) {
-            result.append("\u0003");
-        }
-        if (fgColor != null) {
-            result.append(fgColor);
-        }
-        if (bgColor != null) {
-            result.append("," + bgColor);
-        }
-    }
-
-    private void setState(List<String> state, String value, boolean foreground) {
-        if (Colors.BOLD.equals(value) || Colors.ITALICS.equals(value) || Colors.UNDERLINE.equals(value)) {
-            state.add(0, value);
-        } else {
-            state.add(0, (foreground ? "F_" : "B_") + value.substring(1));
-        }
     }
 
     @Override
     public void sendMessage(String message) {
-        bot.getUserBot().getChannels().forEach(c -> c.send().message(postprocessMessage(message)));
+        postprocessMessage(message).forEach(l -> bot.getUserBot().getChannels().forEach(c -> c.send().message(l)));
+    }
+
+    private List<String> postprocessMessage(String message) {
+        return new MessageParser(message, maxLength).process();
     }
 
     @Override
---
+++ src/main/java/me/rkfg/xmpp/bot/irc/MessageParser.java
@@ -0,0 +1,163 @@
+package me.rkfg.xmpp.bot.irc;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.pircbotx.Colors;
+
+public class MessageParser {
+
+    private static final Map<String, String> REPLACES = new HashMap<>();
+    private StringBuilder sb = new StringBuilder();
+    private List<String> state = new LinkedList<>();
+    private List<String> result = new LinkedList<>();
+    private String message = null;
+    private int i = 0;
+    private int maxLength;
+    private boolean codeFound = false;
+    private boolean needRestore = false;
+    private int lastSpaceIdx = 0;
+
+    public static void init() {
+        REPLACES.put("b", Colors.BOLD);
+        REPLACES.put("i", Colors.ITALICS);
+        REPLACES.put("u", Colors.UNDERLINE);
+        REPLACES.put("white", Colors.WHITE);
+        REPLACES.put("black", Colors.BLACK);
+        REPLACES.put("red", Colors.RED);
+        REPLACES.put("brown", Colors.BROWN);
+        REPLACES.put("magenta", Colors.MAGENTA);
+        REPLACES.put("purple", Colors.PURPLE);
+        REPLACES.put("olive", Colors.OLIVE);
+        REPLACES.put("yellow", Colors.YELLOW);
+        REPLACES.put("green", Colors.GREEN);
+        REPLACES.put("dgreen", Colors.DARK_GREEN);
+        REPLACES.put("gray", Colors.LIGHT_GRAY);
+        REPLACES.put("dgray", Colors.DARK_GRAY);
+        REPLACES.put("blue", Colors.BLUE);
+        REPLACES.put("dblue", Colors.DARK_BLUE);
+        REPLACES.put("teal", Colors.TEAL);
+        REPLACES.put("cyan", Colors.CYAN);
+    }
+
+    public MessageParser(String message, int maxLength) {
+        this.maxLength = maxLength;
+        this.message = message.replace('\n', ' ');
+    }
+
+    public List<String> process() {
+        while (i < message.length()) {
+            final char ch = message.charAt(i);
+            codeFound = false;
+            if (ch == '<') {
+                for (Entry<String, String> r : REPLACES.entrySet()) {
+                    parseOpenTag(r);
+                    parseOpenBGTag(r);
+                    parseCloseTag(r);
+                    parseCloseBGTag(r);
+                }
+            }
+            if (!codeFound) {
+                if (needRestore) {
+                    restoreState();
+                    needRestore = false;
+                }
+                sb.append(ch);
+                if (sb.length() > maxLength && lastSpaceIdx > 0 && lastSpaceIdx < maxLength) {
+                    result.add(sb.substring(0, lastSpaceIdx));
+                    String tail = sb.substring(lastSpaceIdx + 1);
+                    sb = new StringBuilder();
+                    restoreState();
+                    sb.append(tail);
+                    lastSpaceIdx = 0;
+                }
+                if (ch == ' ') {
+                    lastSpaceIdx = sb.length() - 1;
+                }
+                ++i;
+            }
+        }
+        result.add(sb.toString());
+        return result;
+    }
+
+    public void parseCloseBGTag(Entry<String, String> r) {
+        if (message.length() > i + r.getKey().length() + 3 && message.substring(i + 1).toLowerCase().startsWith("/_" + r.getKey() + ">")) {
+            i += r.getKey().length() + 4;
+            removeState(r.getValue(), false);
+            needRestore = true;
+            codeFound = true;
+        }
+    }
+
+    public void parseCloseTag(Entry<String, String> r) {
+        if (message.length() > i + r.getKey().length() + 2 && message.substring(i + 1).toLowerCase().startsWith("/" + r.getKey() + ">")) {
+            i += r.getKey().length() + 3;
+            removeState(r.getValue(), true);
+            needRestore = true;
+            codeFound = true;
+        }
+    }
+
+    public void parseOpenBGTag(Entry<String, String> r) {
+        if (message.length() > i + r.getKey().length() + 2 && message.substring(i + 1).toLowerCase().startsWith("_" + r.getKey() + ">")) {
+            i += r.getKey().length() + 3;
+            sb.append("\u0003" + state.stream().filter(s -> s.startsWith("F_")).findFirst().orElse("99") + "," + r.getValue().substring(1));
+            setState(r.getValue(), false);
+            codeFound = true;
+        }
+    }
+
+    public void parseOpenTag(Entry<String, String> r) {
+        if (message.length() > i + r.getKey().length() + 1 && message.substring(i + 1).toLowerCase().startsWith(r.getKey() + ">")) {
+            i += r.getKey().length() + 2;
+            sb.append(r.getValue());
+            setState(r.getValue(), true);
+            codeFound = true;
+        }
+    }
+
+    private void removeState(String value, boolean foreground) {
+        if (Colors.BOLD.equals(value) || Colors.ITALICS.equals(value) || Colors.UNDERLINE.equals(value)) {
+            state.remove(value);
+        } else {
+            state.remove((foreground ? "F_" : "B_") + value.substring(1));
+        }
+    }
+
+    private void restoreState() {
+        sb.append(Colors.NORMAL);
+        String fgColor = null;
+        String bgColor = null;
+        for (String s : state) {
+            if (fgColor == null && s.startsWith("F_")) {
+                fgColor = s.substring(2);
+            } else if (bgColor == null && s.startsWith("B_")) {
+                bgColor = s.substring(2);
+            } else {
+                sb.append(s);
+            }
+        }
+        if (fgColor != null || bgColor != null) {
+            sb.append("\u0003");
+        }
+        if (fgColor != null) {
+            sb.append(fgColor);
+        }
+        if (bgColor != null) {
+            sb.append("," + bgColor);
+        }
+    }
+
+    private void setState(String value, boolean foreground) {
+        if (Colors.BOLD.equals(value) || Colors.ITALICS.equals(value) || Colors.UNDERLINE.equals(value)) {
+            state.add(0, value);
+        } else {
+            state.add(0, (foreground ? "F_" : "B_") + value.substring(1));
+        }
+    }
+
+}
